bde954f3943cf7a4e3d714c95af8ada7f4fc12f0
jackfrostwillbekingfrost <jack_frost_will_be_king_frost@yahoo.co.jp>
2017-10-17 13:17:58 +0900

    add display object

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    console.log("request");
-    response.end("Hello World!");
+    console.log(JSON.stringify(request));
+    response.end(JSON.stringify(request) + "Hello World!");
 });
 
 var port = process.env.PORT || 1337;
