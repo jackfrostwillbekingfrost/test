acc9d399ec72362713fe1b7d1a796d64d00fb68f
jackfrostwillbekingfrost <jack_frost_will_be_king_frost@yahoo.co.jp>
2017-10-17 12:10:51 +0900

    Change all file to microsoft sample

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Hosomichi!\n');
-}).listen(80);
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
