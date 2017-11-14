bbe7466e828889310303c13d55611627d48f9699
jackfrostwillbekingfrost <jack_frost_will_be_king_frost@yahoo.co.jp>
2017-11-14 18:27:00 +0900

    edit inde.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser');
 
-let data = {};
+let data = "success!";
 
 // app.use(bodyParser.json()); // for parsing JSON
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/text', function(req, res) {
+fs.writeFile('hogehoge', data , function(err){
+    console.log(err);
+});
 res.send(data);
 res.end();
 });
@@ -16,6 +20,7 @@ app.post('/text', function(req, res) {
 console.log(req.body);
 data['text'] = req.body.text;
 res.json({ text: 'Successfuly set the message!' });
+
 res.send(data);
 res.end();
 });
